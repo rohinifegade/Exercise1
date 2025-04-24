2d7571f565b4f75d3a81dcf4733ee9269dd161e6
rohinifegade <rohini.fegade01@gmail.com>
2025-04-24 11:20:27 -0700

    Add files via upload
    Commiting Tasks

---
+++ Task1.py
@@ -0,0 +1,15 @@
+# User Input
+num1=int(input("Enter the first number :"))
+num2=int(input("Enter the second number :"))
+
+# Operations
+addition= num1+num2
+subtraction= num1-num2
+multiplication=num1*num2
+division=num1/num2
+
+# Printing Output
+print(f"Addition:{addition}")
+print(f"Subtraction:{subtraction}")
+print(f"Multiplication:{multiplication}")
+print(f"Division:{division}")
---
+++ Task2.py
@@ -0,0 +1,3 @@
+name1=input("Enter your first name :")
+name2=input("Enter your last name :")
+print("Hello,",name1,name2,"! Welcome to the Python program.")
